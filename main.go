a67c511a71fa4d30a10dde2424c91f64787c9a92
Sean Kenny <sean.kenny@hotmail.com>
2018-08-25 23:22:25 +0100

    Include build information in output

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/CrowdSurge/banner"
 	lol "github.com/kris-nova/lolgopher"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	fmt.Printf("%s %s %s", date, version, commit)
+
 	w := lol.NewLolWriter()
 	w.Write([]byte(banner.PrintS("hello go modules")))
 }
